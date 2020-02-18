6f270e07191950d697f68ff964fbe28fc01308b0
Jonathan <jonathan_guerreiro@hotmail.com>
2020-02-18 18:58:00 -0300

    plantillas de registro, login y el css principal

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const express = require("express"),
 mongoose.connect("mongodb://localhost:2717/test", {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
 	res.render("login");
+});
+
+app.get("/register", (req, res) => {
+	res.render("register");
 })
 
 app.listen(3000, () => console.log("Server is up!"));

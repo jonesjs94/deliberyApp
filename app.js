353714a76eb561c9a68085f95180b313d21917c8
Jonathan <jonathan_guerreiro@hotmail.com>
2020-02-25 17:50:09 -0300

    interfaz de negocio agregada, font awesome en header

--- app.js
+++ app.js
@@ -7,12 +7,29 @@ mongoose.connect("mongodb://localhost:2717/test", {useNewUrlParser: true, useUni
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
+
+// LOGIN ROUTES
+
 app.get("/", (req, res) => {
-	res.render("login");
+	res.redirect("negocios");
 });
 
-app.get("/register", (req, res) => {
-	res.render("register");
+app.get("/entrar", (req, res) => {
+	res.render("entrar");
+});
+
+app.get("/registrarse", (req, res) => {
+	res.render("registrarse");
+})
+
+// BUSINESS ROUTES
+
+app.get("/negocios", (req, res) => {
+	res.render("negocios");
+});
+
+app.get("/negocio", (req, res) => {
+	res.render("negocio");
 })
 
 app.listen(3000, () => console.log("Server is up!"));
